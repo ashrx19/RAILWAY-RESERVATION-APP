b3e302c9ea2df6ed8090c56358bc7efaca309c2c
ashrx19 <ashwin191205@gmail.com>
2026-08-15 14:45:00 +0530

    fix: resolve booking workflow issues

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,7 @@ import AdminDashboard from './pages/AdminDashboard';
 import BookTrain from './pages/BookTrain';
 import GoogleCallback from './pages/GoogleCallback';
 import './App.css';
-\\\\\\\\\\\\\\\\\
-
-
-
+\\\\\\\\
 
 
 
